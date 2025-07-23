b06d708f1e8c93730f1fa6a0302a23e2a021059b
meghakrishnanan <23pa5a0515@vishnu.edu.in>
2025-07-23 09:45:48 +0530

    Update and rename Unit 1 to 10.rs

---
+++ 10.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let str_literal = "Hello";
+    println!("String literal: {}", str_literal);
+    let str_obj = String::from(str_literal);
+    println!("String object: {}", str_obj);
+}
